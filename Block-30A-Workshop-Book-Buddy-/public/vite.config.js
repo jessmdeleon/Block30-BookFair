08c4bf10690f48ec61d69973f9111de410509438
jessmdeleon <deleon.jessica060@gmail.com>
2024-09-29 19:26:47 -0400

    update and move files

--- Block-30A-Workshop-Book-Buddy-/public/vite.config.js
+++ Block-30A-Workshop-Book-Buddy-/public/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.', // Use `.` if `vite.config.js` is in the same folder as `index.html`
+  root: 'index.html', // Use `.` if `vite.config.js` is in the same folder as `index.html`
   plugins: [react()],
   build: {
     rollupOptions: {
