b869cc14bb89b556ac6135c1e2b3881f20318567
jessmdeleon <deleon.jessica060@gmail.com>
2024-09-29 18:22:35 -0400

    Fix Vite configuration for entry module

--- Block-30A-Workshop-Book-Buddy-/vite.config.js
+++ Block-30A-Workshop-Book-Buddy-/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './', 
+  root: '.', // Use `.` if `vite.config.js` is in the same folder as `index.html`
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: './index.html' 
+      input: './index.html' // Adjust path relative to `vite.config.js`
     }
   }
 });
