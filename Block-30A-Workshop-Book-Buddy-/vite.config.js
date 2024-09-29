db05aa29c3f0deed5730297ddb00890f992c27ef
jessmdeleon <deleon.jessica060@gmail.com>
2024-09-29 17:57:09 -0400

    update files

--- Block-30A-Workshop-Book-Buddy-/vite.config.js
+++ Block-30A-Workshop-Book-Buddy-/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'Block-30A-Workshop-Book-Buddy-',
+  root: 'Block-30A-Workshop-Book-Buddy-', 
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'Block-30A-Workshop-Book-Buddy-/index.html' 
+    }
+  }
 });
