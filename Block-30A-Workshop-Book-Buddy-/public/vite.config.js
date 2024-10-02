4c7687d5176c72d40b130e1b815e0d5aa63030f4
jessmdeleon <deleon.jessica060@gmail.com>
2024-10-02 13:22:47 -0400

    file updated

--- Block-30A-Workshop-Book-Buddy-/public/vite.config.js
+++ Block-30A-Workshop-Book-Buddy-/public/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  root: 'index.html', // Use `.` if `vite.config.js` is in the same folder as `index.html`
-  plugins: [react()],
+export default {
   build: {
     rollupOptions: {
-      input: './index.html' // Adjust path relative to `vite.config.js`
-    }
-  }
-});
+      external: ["/src/main.jsx"],
+    },
+  },
+};
